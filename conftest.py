e488a1cefcde7df8897f104770d73f4f29590381
klimashko <klimashko108@gmail.com>
2023-02-23 23:11:10 +0300

    Убрал из тестов открытие браузера и перенес его в фикстуру, изменил имя фикстуры, добавив open

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selene.support.shared import browser
 
 
 @pytest.fixture
-def set_size_browser_window():
+def set_size_browser_window_open():
     browser.config.window_width = 500
     browser.config.window_height = 626
+    browser.open('https://duckduckgo.com/')
--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,19 +2,16 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_search_text(set_size_browser_window):
-    browser.open('https://duckduckgo.com/')
+def test_search_text(set_size_browser_window_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[class="serp__results js-serp-results"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_another_text_search(set_size_browser_window):
-    browser.open('https://duckduckgo.com/')
+def test_another_text_search(set_size_browser_window_open):
     browser.element('[name="q"]').should(be.blank).type('oifviwurh834764786odkjfbwhi').press_enter()
     browser.element('[class="serp__results js-serp-results"]').should(have.text('oifviwurh834764786odkjfbwhi'))
 
 
-def test_text_random_search(set_size_browser_window):
-    browser.open('https://duckduckgo.com/')
+def test_text_random_search(set_size_browser_window_open):
     browser.element('[name="q"]').should(be.blank).type('щавомрывшщармшг834678347ваомиодрм').press_enter()
     browser.element('[class="no-results__title"]').should(have.text('По запросу щавомрывшщармшг834678347ваомиодрм результаты не найдены.'))
